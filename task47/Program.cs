a67e37d049d03ab0551946da9cce474c46c4ac7e
Kalybaev Rustam <me@kalybaev.ru>
2023-05-16 00:23:09 +0500

    task 47 done

---
+++ task47/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите m массива: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите n массива: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+Random rand = new Random();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = rand.NextDouble() * 200 - 100;
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{array[i, j]:F2}\t");
+    }
+    Console.WriteLine();
+}
